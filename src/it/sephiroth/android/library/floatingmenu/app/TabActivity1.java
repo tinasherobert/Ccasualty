b7837c3ee29575a52c0b197fbb8c5661c0dc2d0c
hopewell <hopewell@astro.co.zw>
2014-07-30 12:36:30 +0200

    the app
    files

---
+++ src/it/sephiroth/android/library/floatingmenu/app/TabActivity1.java
@@ -0,0 +1,19 @@
+
+package it.sephiroth.android.library.floatingmenu.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+
+public class TabActivity1  extends Activity
+{
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+           
+            
+            setContentView(R.layout.activity_tabs);
+        }
+}
