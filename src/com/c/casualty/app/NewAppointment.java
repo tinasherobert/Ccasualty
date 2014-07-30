60298e8038269fcba8cb2f7b6decfd3e804eb227
Tinashe Makaza <makazatinashe2000@gmail.com>
2014-07-30 17:46:34 +0200

    Ma com changes
    com.c.casualty.app

---
+++ src/com/c/casualty/app/NewAppointment.java
@@ -0,0 +1,75 @@
+package com.c.casualty.app;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.graphics.Point;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+
+@SuppressLint("ValidFragment")
+public class NewAppointment extends DialogFragment {
+
+	
+
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		if (getDialog() != null) {
+			getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+			getDialog().getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+		}
+
+		View root = inflater.inflate(R.layout.popview_layout, container, false);
+
+		return root;
+	}
+	@Override
+	public void onActivityCreated(Bundle arg0) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(arg0);
+			
+	}
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		// change dialog width
+		if (getDialog() != null) {
+
+			int fullWidth = getDialog().getWindow().getAttributes().width;
+
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+				Display display = getActivity().getWindowManager().getDefaultDisplay();
+				Point size = new Point();
+				display.getSize(size);
+				fullWidth = size.x;
+			} else {
+				Display display = getActivity().getWindowManager().getDefaultDisplay();
+				fullWidth = display.getWidth();
+			}
+
+			final int padding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, getResources()
+					.getDisplayMetrics());
+
+			int w = fullWidth - padding;
+			int h = getDialog().getWindow().getAttributes().height;
+
+			getDialog().getWindow().setLayout(w, h);
+		}
+	}
+
+
+}
---
+++ src/com/c/casualty/app/ScrollViewActivity.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2013 Manuel Peinado
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.c.casualty.app;
+
+import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+import com.manuelpeinado.fadingactionbar.FadingActionBarHelper;
+
+public class ScrollViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FadingActionBarHelper helper = new FadingActionBarHelper()
+        .actionBarBackground(R.drawable.ab_background)
+        .headerLayout(R.layout.header)
+        .contentLayout(R.layout.activity_scrollview)
+       ;
+	    setContentView(helper.createView(this));
+	    helper.initActionBar(this);
+	   // TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
+	
+	
+//	    TabSpec tab1 = tabHost.newTabSpec("First Tab");
+//	    TabSpec tab2 = tabHost.newTabSpec("Second Tab");
+//	    TabSpec tab3 = tabHost.newTabSpec("Third tab");
+//	
+//	  
+//	    tab1.setIndicator("Tab1");
+//	    tab1.setContent(new Intent(this,TabActivity1.class));
+//	    
+//	    tab2.setIndicator("Tab2");
+//	    tab2.setContent(new Intent(this,TabActivity2.class));
+//	
+//	    tab3.setIndicator("Tab3");
+//	    tab3.setContent(new Intent(this,TabActivity3.class));
+//	    
+//	    
+//	    tabHost.addTab(tab1);
+//	    tabHost.addTab(tab2);
+//	    tabHost.addTab(tab3);
+    }
+
+   
+}
